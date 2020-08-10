ace7c978b3ab3afb1c387549a73c82e395d9af29
rkstrdee <rkstrdee@gmail.com>
2020-08-10 16:44:48 +0530

    fluent design effect

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,12 +16,53 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route('/')
+db.create_all()
+db.session.commit()
+
+@app.route('/', methods=['POST','GET'])
 
 
 
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content'] #name of the form
+        new_task = Todo(content = task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/') #redirect to the main page
+        except:
+            return 'There was an issue adding your task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem  deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET','POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue updating your task"
+    else:
+        return render_template('update.html', task = task)
 
 
 if __name__ == "__main__":
